df030c235c5294128fbf7c9cde4551007f807329
Taysaun <taysa25495@live.ytech.edu>
2024-08-26 14:58:17 -0400

    start calc

---
+++ docpac_02300824/jscalc/script.js
@@ -0,0 +1,10 @@
+const num1 = document.getElementById('number1').value;
+const num2 = document.getElementById('number2').value;
+const box = document.getElementById('box');
+const options = document.getElementById('arithmetic').value;
+const result = document.getElementById('result');
+
+result.addEventListener('click', () => {
+    console.log(parseInt(num1))
+})
+
