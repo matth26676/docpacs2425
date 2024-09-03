268e924d086d0815b8f57a97045a3e6c3d4559a4
kris26658 <129105342+kris26658@users.noreply.github.com>
2024-09-03 10:05:06 -0400

    stuck
    I'm at a loss at to what to do from here

---
+++ docpac_03060924/krisBowman/nodeTest.js
@@ -0,0 +1,14 @@
+const express=require("express"); // add express module
+const {send} = require("process");
+
+const app=express(); //transfer express into object for furture usage
+const PORT = 3000;
+
+app.listen(PORT, (req, res) => {
+    console.log("app started");
+
+app.get("/", (req, res,)) => {
+
+});   send 
+
+});
