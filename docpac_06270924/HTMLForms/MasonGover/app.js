8bfc49ad8f63e72b712c010ef726fa72f16e705e
mason26611 <123019028+mason26611@users.noreply.github.com>
2024-09-24 14:50:00 -0400

    Got data working with body-parser

--- docpac_06270924/HTMLForms/MasonGover/app.js
+++ docpac_06270924/HTMLForms/MasonGover/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
+function getData() {
+	return JSON.parse(fs.readFileSync("data.json")) || {};
+}
+
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
 	res.render("index.ejs");
 });
@@ -11,13 +19,17 @@ app.get("/add", (req, res) => {
 	res.render("add.ejs");
 });
 
-app.get("/view", (req, res) => {
-	res.render("view.ejs");
+app.post("/add", (req, res) => {
+	try {
+		console.log(req.body);
+	} catch(err) {
+		res.render("error.ejs", { error: err });
+	}
 });
 
-app.post("/view", (req, res) => {
-	console.log(req, res);
-	res.send({message: "Data received"});
+app.get("/view", (req, res) => {
+	const data = getData();
+	res.render("view.ejs", { data });
 });
 
 app.listen(port);
