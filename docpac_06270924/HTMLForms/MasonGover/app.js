2ae89ac4f24f327dfc652ac877d3b111e429ca0f
mason26611 <123019028+mason26611@users.noreply.github.com>
2024-09-24 22:20:09 -0400

    Fix form, made it display data, and add checks

--- docpac_06270924/HTMLForms/MasonGover/app.js
+++ docpac_06270924/HTMLForms/MasonGover/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 const app = express();
 const port = 3000;
+const expectedData = [
+	"firstName",
+	"lastName",
+	"password",
+	"confirmPassword",
+	"favoriteColor",
+	"leastFavoriteColor",
+	"skibidiLevel",
+	"sodaLikeness",
+	"isSigma",
+	"likesSoda"
+]
 
 function getData() {
 	return JSON.parse(fs.readFileSync("data.json")) || {};
 }
 
-app.use(bodyParser.json());
+// Convert from a URL format to a JSON format
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -21,15 +33,31 @@ app.get("/add", (req, res) => {
 
 app.post("/add", (req, res) => {
 	try {
-		console.log(req.body);
+		const body = req.body;
+		const newData = { data: [] };
+		for (const bodyData in body) {
+			if (!expectedData.includes(bodyData)) {
+				throw new Error(`Unexpected data: ${bodyData}`);
+			}
+		}
+
+		for (const expectedData in expectedData) {
+			if (!body[expectedData]) {
+				throw new Error(`Missing data: ${expectedData}`);
+			}
+		}
+
+		newData.data.push(body);
+		fs.writeFileSync("./data.json", JSON.stringify(newData));
+		res.redirect("/");
 	} catch(err) {
 		res.render("error.ejs", { error: err });
 	}
 });
 
 app.get("/view", (req, res) => {
-	const data = getData();
-	res.render("view.ejs", { data });
+	const jsonData = getData();
+	res.render("view.ejs", { data: jsonData.data[0] });
 });
 
 app.listen(port);
