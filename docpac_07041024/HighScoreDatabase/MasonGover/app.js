4a07a4153a61a413483af254bb0914f862b7e021
mason26611 <masong1508paypal@gmail.com>
2024-10-01 09:30:05 -0400

    Made database & ejs files work

--- docpac_07041024/HighScoreDatabase/MasonGover/app.js
+++ docpac_07041024/HighScoreDatabase/MasonGover/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const sqlite = require("sqlite3");
+const sqlite = require("sqlite3").verbose();
 const port = 3000;
 const app = express();
 const db = new sqlite.Database("database.db");
@@ -13,27 +13,51 @@ app.get("/", (req, res) => {
 });
 
 app.get("/game", (req, res) => {
-    res.render("/game.ejs");
+    res.render("game.ejs");
 });
 
 app.get("/hiscores", (req, res) => {
-    // todo
+    try {
+        db.all("SELECT * FROM scores", (err, rows) => {
+            if (err) {
+                throw new Error("An error occurred accessing the database.")
+            }
+    
+            // Sort the scores & get the top 10 then display them
+            rows.sort();
+            rows.reverse();
+            rows = rows.slice(0, 10);
+            res.render("hiscores.ejs", { scores: rows });
+        });
+    } catch (err) {
+        res.render("error.ejs", { error: err.message });
+    }
 });
 
 app.post("/hiscores", (req, res) => {
     try {
-        // Old code
-        // const body = req.body;
-        // if (body.score === undefined) {
-        //     return res.status(400).send("Score is required");
-        // }
+        throw new Error("nop")
+        const body = req.body;
+        if (body.score === null || body.name === null) {
+            throw new Error("Score and name is required.")
+        }
 
-        // const scoreData = JSON.parse(fs.readFileSync("./scores.json"));
-        // scoreData[req.ip] = body.score;
-        // fs.writeFileSync("./scores.json", JSON.stringify(scoreData));
+        const ip = req.ip;
+        db.run("INSERT INTO scores (ip, name, score) VALUES (?, ?, ?)", [ip, body.name, body.score], (err) => {
+            if (err) {
+                throw new Error("An error occurred accessing the database.")
+            }
+
+            // The score was added successfully
+            res.status(200).send();
+        });
     } catch (err) {
-        res.status(500).send("An error occurred");
+        res.status(500).send();
     }
+});
+
+app.get("/error", (req, res) => {
+    res.render("error.ejs")
 });
 
 app.listen(port);
