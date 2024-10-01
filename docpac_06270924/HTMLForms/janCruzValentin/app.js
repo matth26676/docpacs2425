808167db02d9128a1b289a5dec1a43515403778f
Jan26915 <Jan26915@live.ytech.edu>
2024-10-01 17:00:48 -0400

    Updating files
    Personalizing the files

--- docpac_06270924/HTMLForms/janCruzValentin/app.js
+++ docpac_06270924/HTMLForms/janCruzValentin/app.js
@@ -19,6 +19,7 @@ app.get('/', (req, res) => {
 app.get('/view', (req, res) => {
     const data = JSON.parse(fs.readFileSync('./data.json')).data;
 
+    //views the current array of data in orders
     res.render('view', { orders: data });
 });
 
@@ -36,12 +37,14 @@ app.post('/add', (req, res) => {
             quantity: req.body.quantity
         };
         
+        //Brand by default can never as it always has the first option from the drop down menu initially but for saftey
         if (orderData.brand === '') throw new Error('Brand is required');
         if (!orderData.quantity) throw new Error('Quantity required');
-        if (orderData.quantity > 2000) throw new Error('Too much soda');
+        if (orderData.quantity > 2000) throw new Error('You\'re putting us out of stock');
 
+        //pushing the information in orderData pulled from the info in add into ./data.json
         console.log(orderData)
-        data.push(orderData),
+        data.push(orderData);
 
         fs.writeFileSync('./data.json', JSON.stringify({data: data}));
         
