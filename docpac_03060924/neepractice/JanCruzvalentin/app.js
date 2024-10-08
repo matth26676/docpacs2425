a3638a38217a6658368c6c679bf7e738b6320cf2
Jan26915 <Jan26915@live.ytech.edu>
2024-10-08 13:48:10 -0400

    Random stuff from week 3
    Never updated

--- docpac_03060924/neepractice/JanCruzvalentin/app.js
+++ docpac_03060924/neepractice/JanCruzvalentin/app.js
@@ -3,19 +3,14 @@ const app = express();
 
 // configures express to use ejs 
 app.set('view engine', 'ejs');
-
-// creates http listen server w express
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 // creates a get endpoint
 app.get('/', (req, res) => {
     res.send('Welcome to my app');
 });
 
-// the other endpoint 
+// the endpoint
 app.get('/endpoint', (req, res) => {
     let name = 'Guest'; 
     if (req.query.name) {
@@ -27,4 +22,7 @@ app.get('/endpoint', (req, res) => {
     res.render('hello', { name: Jan });
 });
 
-app.listen(PORT)
+//Starting the server 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
