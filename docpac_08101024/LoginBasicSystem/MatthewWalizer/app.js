dbf024b1f08f00ee989ca27cc75e68b9dacadc03
matth26676 <123019134+matth26676@users.noreply.github.com>
2024-10-18 12:58:55 -0400

    Basic Login System
    The completed code for the Basic Login System

--- docpac_08101024/LoginBasicSystem/MatthewWalizer/app.js
+++ docpac_08101024/LoginBasicSystem/MatthewWalizer/app.js
@@ -1,110 +1,114 @@
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3');
 const express = require('express');
-const { error, log } = require('console');
-const fs = require('fs')
-const crypto = require("crypto");
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
 
-// sets app.js to use expressjs files on port 3000
 const app = express();
-const port = 3000;
-const key = crypto.createCipher('aes-128-cbc', "P@$$w0rdK3y");
+const AUTH_URL = ''
+const THIS_URL = ''
 
-// sets app to view files in ejs and to respond with text
 app.set('view engine', 'ejs');
-app.use(express.text())
-app.use(express.urlencoded({extended:true}));
 
-// sets the port used to port 3000
-app.set('port', process.env.PORT || 3000);
+app.use(express.urlencoded({ extended: true }))
 
-// logs each request
-app.use((req, res, next) => {
-    console.log(`${req.method} request for ${req.url}`);
-    next();
-});
+app.use(session({
+    secret: 'Temp',
+    resave: false,
+    saveUninitialized: false
+}))
 
-// starts the server
-app.listen(port, (err) => {
+function isAuthenticated(req, res, next) {
+    if (req.session.user) next()
+    else res.redirect('/login')
+};
+
+app.listen(3000, (err) => {
     if (err) {
-        console.error(err);
+        console.log(err)
     } else {
-        console.log(`Server listening on port ${port}`);
+        console.log('app.js is running on port 3000')
     }
-});
+})
 
-let db = new sqlite3.Database('data/Database.db', (err) => {
+const db = new sqlite3.Database('data/Database.db', (err) => {
     if (err) {
-        console.error(err.message);
+        console.log(err);
+    } else {
+        console.log('Connected to the data/Database.db!')
     }
-    console.log('Connected to the data/Database database')
 });
 
 app.get('/', (req, res) => {
-    res.render('index', {key: key})
-})
-
-app.get('/signup', (req, res) => {
-    res.render('signup')
+    res.render('index')
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.get('/home', (req, res) => {
-    var user = req.query.user
-    var email = req.query.email
-    res.render('home', {user: user, email: email})
-})
-
-app.get('/error', (req, res) => {
-    res.render('error', {err: err})
-})
-
-app.post('/login', (req, res) => {
+app.get('/home', isAuthenticated, (req, res) => {
     try {
-        if (!req.body.name) {
-            throw Error("Username isn't valid!")
-        } else if (!req.body.email) {
-            throw Error("Email isn't valid!")
-        } else if (!req.body.password) {
-            throw Error("Password isn't valid!")
-        } else {
-            let password = key.update(req.body.password, 'utf8', 'hex').toString();
-            let hexpassword = key.final('hex').toString()
-            db.all(`SELECT * FROM Users WHERE Username=? AND Password=?`, req.body.name, hexpassword, (err, row) => {
-                if (err) {
-                    res.render('error', {err: err})
-                } else {
-                    res.redirect(`home?user=${req.body.name}&email=${req.body.email}`)
-                }
-            })
-        }
-    } catch (err) {
-        res.render('error', {err: err})
+        res.render('home', { user: req.session.user })
+    }
+    catch (error) {
+        res.send(error.message)
     }
 })
 
-app.post('/signup', (req, res) => {
-    try {
-        if (!req.body.name) {
-            throw Error("Username isn't valid!")
-        } else if (!req.body.email) {
-            throw Error("Email isn't valid!")
-        } else if (!req.body.password) {
-            throw Error("Password isn't valid!")
-        } else {
-            let password = key.update(req.body.password, 'utf8', 'hex').toString();
-            let hexpassword = key.final('hex').toString()
-            db.run(`INSERT INTO Users(Username, Email, Password) VALUES(?, ?, ?)`,[req.body.name, req.body.email, hexpassword] ,(err, row) => {
-                if (err) {
-                    res.render('error', {err: err})
-                } else {
-                    res.render('login')
-                }
-            })
-        }
-    } catch (err) {
-        res.render('error', {err: err})
+app.get('/error', (req, res) => {
+
+})
+
+// POST requests
+
+app.post('/login', (req, res) => {
+    if (req.body.user && req.body.pass && req.body.email) {
+        db.get(`SELECT * FROM Users WHERE Username=?;`, req.body.user, (err, row) => {
+            if (err) {
+                console.log(err);
+                res.send('Whoops! Something went wrong :(');
+            } else if (!row) {
+                // Create a new salt(key) for the user
+                const salt = crypto.randomBytes(16).toString('hex')
+
+                // Uses salt to hash password
+                crypto.pbkdf2(req.body.pass, salt, 1000, 64, 'sha512', (err, derivedKey) => {
+                    if (err) {
+                        console.log(err)
+                        res.send('Whoops! Something went wrong :(')
+                    } else {
+                        const hashedPassword = derivedKey.toString('hex')
+                        db.run(`INSERT INTO Users(Username, Email, Password, Salt) VALUES(?, ?, ?, ?)`, [req.body.user, req.body.email, hashedPassword, salt], (err) => {
+                            if (err) {
+                                console.log(err);
+                                res.send('Whoops! Something went wrong :(')
+                            } else {
+                                res.send("Created New User!");
+                            }
+                        })
+                    }
+                })
+            } else if (row) {
+                // compares stored password with provided password
+                crypto.pbkdf2(req.body.pass, row.salt, 1000, 64, 'sha512', (err, derivedKey) => {
+                    if (err) {
+                        res.send('Whoops! Something went wrong :(')
+                    } else {
+                        const hashedPassword = derivedKey.toString('hex')
+                        if (row.Password === hashedPassword) {
+                            req.session.user = req.body.user;
+                            res.redirect('/home');
+                        } else {
+                            res.render("Incorrect Password Please check for errors in password...")
+                        }
+                    }
+                })
+
+
+            }
+        })
+    } else {
+        res.send("You need a valid username, email, and password")
     }
 })
