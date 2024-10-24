090f8df8a7400f9e20735f49ec41397b33495665
isabellafeliciano1 <isabe25578@live.ytech.edu>
2024-10-24 11:53:44 -0700

    FormbarOauth2 and CIW4

--- docpac_08101024/loginBoilerPlate/IsabellaFeliciano/app.js
+++ docpac_08101024/loginBoilerPlate/IsabellaFeliciano/app.js
@@ -1,3 +1,4 @@
+
 const sqlite3 = require('sqlite3');
 const express = require('express');
 const crypto = require('crypto');
@@ -109,3 +110,6 @@ app.get('/home', isAuthenticated, (req, res) => {
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
+
+
+
