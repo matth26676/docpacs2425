c053415a7df3e8932dd9a186ecc81771cc897a03
Conno26678 <conno26678@live.ytech.edu>
2024-10-31 11:20:32 -0400

    work again

--- docpac_10011124/websocketsChat/ConnorYeager/app.js
+++ docpac_10011124/websocketsChat/ConnorYeager/app.js
@@ -4,14 +4,29 @@ const http = require('http').Server(app);
 const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ server: http });
 const { v4: uuidv4 } = require('uuid');
-const clients = new Map();
+let clients = new Map();
+let messages = [];
 
 app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 function broadcast(ws, message) {
-    
+    console.log(message)
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify(message));
+    });
 };
+function userList(wss){
+    let list = [];
+    wss.clients.forEach((client, index) => {
+        if(client.hasOwnProperty('name')){
+            list.push(client);
+        }
+    });
+
+    return {list: list};
+}
 
 wss.on('connection', (ws) => {
     const id = uuidv4();
@@ -22,7 +37,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/chat', (req, res) => {
-    res.render('chat');
+    if (!req.query.name) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('chat', { name: req.query.name });
 })
 
 http.listen(3000, () => { console.log(`Server started on http://localhost:3000`); });
