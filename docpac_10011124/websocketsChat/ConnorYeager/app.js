36b44d8d77edbdd147bf26df1e29f09804525aaf
Conno26678 <conno26678@live.ytech.edu>
2024-10-31 10:07:39 -0400

    working on 10

--- docpac_10011124/websocketsChat/ConnorYeager/app.js
+++ docpac_10011124/websocketsChat/ConnorYeager/app.js
@@ -1,10 +1,21 @@
-express = require('express');
-
-const webserver = express();
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const { WebSocketServer } = require('ws');
+const wss = new WebSocketServer({ server: http });
+const { v4: uuidv4 } = require('uuid');
+const clients = new Map();
 
 app.set('view engine', 'ejs');
-app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+function broadcast(ws, message) {
+    
+};
+
+wss.on('connection', (ws) => {
+    const id = uuidv4();
+});
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -12,4 +23,6 @@ app.get('/', (req, res) => {
 
 app.get('/chat', (req, res) => {
     res.render('chat');
-});
+})
+
+http.listen(3000, () => { console.log(`Server started on http://localhost:3000`); });
