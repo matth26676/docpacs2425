6d772e17984ab2a8a11a7e33f9535b5fa193166a
GEbanks <167536152+GEbanks@users.noreply.github.com>
2024-10-31 11:20:52 -0400

    working on 010

--- docpac_10011124/Websocket/GabrielEbanks/app.js
+++ docpac_10011124/Websocket/GabrielEbanks/app.js
@@ -1,14 +1,36 @@
-const {  log  } = require('console');
+const { json, text } = require('body-parser');
+const { log } = require('console');
+const express = require('express')
+const app = express()
+const { v4:uuidv4} = require('uuid')
+const { WebSocketServer } = require('ws')
+const wss = new WebSocketServer({ port: 443 })
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }))
+
+const http = require('http').Server(app);
+http.listen(3000, () => { console.log(`Server started on http://localhost:3000`); });
+
 
 app.get('/', (req, res) => {
     res.render('index'); // Renders the index view
+    
 });
 
 
 app.get('/chat', (req, res) => {
-    res.render('chat'); // Renders the index view
+    if (req.query.username) {
+        res.render('chat', {name: req.query.username}); // Renders the index view
+    } else {
+        res.render('index'); // Renders the index view
+    }
 });
 
+function broadcast (wss,message) {
+    
+}
 
 
 
