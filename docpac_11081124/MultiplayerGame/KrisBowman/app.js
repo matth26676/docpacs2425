7154263c3da6d16c3eaf138551b7e9b9b58c4cdf
kris26658 <129105342+kris26658@users.noreply.github.com>
2024-11-04 11:18:14 -0500

    edits1

--- docpac_11081124/MultiplayerGame/KrisBowman/app.js
+++ docpac_11081124/MultiplayerGame/KrisBowman/app.js
@@ -1,4 +1,4 @@
-//to install required modules, in terminal: "npm i express ejs ws http"
+//to install required modules, in terminal: "npm i express ejs io.io http"
 
 const express = require("express"); //import express
 const app = express(); //initialize express with app
@@ -14,66 +14,130 @@ HTTP Server
 
 const http = require('http').Server(app); //import http, create http server and associate it with express
 
-const PORT = process.env.PORT || 1000; //change port number from default
+const PORT = process.env.PORT || 6473; //change port number from default
 http.listen(PORT, console.log(`Server started on port ${PORT}`)); //start http server, listen on given port
 
 /*--------------
-WebSocket Server
+socket.io Server
 --------------*/
 
-const WebSocket = require("ws"); //import WebSocket
-const wss = new WebSocket.Server({ server: http }); //create new WebSocket server, attach it to http server
+const io = require("socket.io"); //import io.io
+const sio = new io.Server({ server: http }); //create new io.io server, attach it to http server
 
-function broadcast(wss, data) {
-    const message = JSON.stringify(data);
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        };
+sio.on("connection", (io => { //on connection to Webio server
+    io.on("close", () => console.log(`${io.id} has disconnected.`)); //user disconnects
+    userList(sio); //reload user list
+
+    io.on("connection", (io) => {
+        console.log("User connected.");
     });
-};
 
-function userList(wss) {
-    let users = [];
-    wss.clients.forEach(client => {
-        if (client.name) {
-            users.push(client.name);
-        };
-    });
-    return { list: users };
-};
+    io.on("move" => {
 
-wss.on("connection", (ws => { //on connection to WebSocket server
-    ws.on("close", () => console.log(`${ws.name} has disconnected.`)); //user disconnects
-    broadcast(wss, userList(wss)); //reload user list
-
-    ws.on("message", (data) => {
-        const parsedMsg = JSON.parse(data); //parse incoming message
-
-        if (parsedMsg.name) {
-            ws.name = parsedMsg.name;
-            broadcast(wss, userList(wss));
-        };
-        if (parsedMsg.text) {
-            broadcast(wss, { user: ws.name, text: parsedMsg.text });
-        };
     });
 }));
 
+/*----
+Canvas
+----*/
+const cv = document.getElementById("cv")
+const ctx = cv.getContext("2d");
+
+/*-----
+Players
+-----*/
+
+//create player object
+const PLAYER = {};
+
+//properties for player
+PLAYER.id += 1
+PLAYER.x = 400;
+PLAYER.y = 300;
+PLAYER.w = 25;
+PLAYER.h = 50;
+
 /*---------------
-GET/POST Requests
+keyboard controls
 ---------------*/
 
-//handle index
-app.get("/", (req, res) => {
-    res.render("index");
-});
+//events
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-//handle chat
-app.get("/chat", (req, res) => {
-    const name = req.query.name;
-    if (!name) {
-        return res.redirect("/");
+//direction keys
+let wDown = false;
+let sDown = false;
+
+//set keys to true when pressed
+function keyDownHandler(e) {
+    if (e.code === "KeyW") {
+        wDown = true;
+    } else if (e.code === "KeyS") {
+        sDown = true;
     };
-    res.render("chat", { name });
-});
+};
+function keyUpHandler(e) {
+    if (e.code === "KeyW") {
+        wDown = false;
+    } else if (e.code === "KeyS") {
+        sDown = false;
+    };
+};
+
+//move player on key down
+if (sDown) {
+    PLAYER.y += 5;
+} else if (wDown) {
+    PLAYER.y -= 5;
+};
+
+//player cannot leave canvas
+if (PLAYER.x < 0) {
+    PLAYER.x = 0
+} else if (PLAYER.y < 0) {
+    PLAYER.y = 0
+};
+
+if (PLAYER.x + PLAYER.w > cv.width) {
+    PLAYER.x = cv.width - PLAYER.w
+} else if (PLAYER.y + PLAYER.h > cv.height) {
+    PLAYER.y = cv.height - PLAYER.h
+};
+
+    /*--
+    Draw
+    --*/
+
+    function draw() {
+        ;
+        //erase the screen
+        ctx.clearRect(0, 0, cv.width, cv.height);
+
+        //draw the player
+        ctx.drawImage(PLAYER.img, PLAYER.x, PLAYER.y, PLAYER.w, PLAYER.h);
+
+        //text in top left
+        ctx.strokeText("keyboard (wasd)", 0, 10);
+        ctx.strokeText("x: " + PLAYER.x, 0, 20);
+        ctx.strokeText("y: " + PLAYER.y, 0, 30);
+    };
+
+    function loop() {
+        update();
+        draw();
+        requestAnimationFrame(loop);
+    };
+
+    PLAYER.img.onload = function () {
+        loop();
+    };
+
+    /*---------------
+    GET/POST Requests
+    ---------------*/
+
+    //handle index
+    app.get("/", (req, res) => {
+        res.render("game");
+    });
