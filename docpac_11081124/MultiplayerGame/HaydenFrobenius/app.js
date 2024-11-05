2577a6cfca821abd20d6f211f8182ce46f1f10c6
hayde26904 <56613295+hayde26904@users.noreply.github.com>
2024-11-05 13:21:49 -0500

    started implementing multiplayer

--- docpac_11081124/MultiplayerGame/HaydenFrobenius/app.js
+++ docpac_11081124/MultiplayerGame/HaydenFrobenius/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 let serv = require('http').Server(app);
+const crypto = require('crypto');
+
+function generateKey() {
+  return crypto.randomBytes(2).toString('hex').slice(0, 4);
+}
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
@@ -9,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 serv.listen(PORT);
+
+const ROWS = 6;
+const COLS = 7;
 
 let games = new Map();
 
@@ -27,5 +35,20 @@ io.sockets.on('connection', function (socket) {
 });
 
 app.get('/', (req, res) => {
-    res.render('game');
+    res.render('index');
+});
+
+app.get('/joingame', (req, res) => {
+    res.render('joingame');
+});
+
+app.get('/creategame', (req, res) => {
+    let gameCode = generateKey();
+    let game = {
+        code: gameCode,
+        player1: null,
+        player2: null,
+        board: new Array(ROWS).fill(0).map(() => new Array(COLS).fill(0)),
+        turn: 1,
+    };
 });
