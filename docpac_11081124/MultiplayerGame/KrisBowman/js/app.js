0f13230d19965ccb5b4888df712cccdf7e294281
kris26658 <129105342+kris26658@users.noreply.github.com>
2024-11-07 09:51:49 -0500

    should be working but can't test
    file location errors

--- docpac_11081124/MultiplayerGame/KrisBowman/js/app.js
+++ docpac_11081124/MultiplayerGame/KrisBowman/js/app.js
@@ -1,14 +1,11 @@
-//to install required modules, in terminal: "npm i express ejs path socket.io http"
+//to install required modules, in terminal: "npm i express path http socket.io"
 
 const express = require("express"); //import express
 const app = express(); //initialize express with app
 
-const ejs = require("ejs"); //import ejs
-const path = require("path");
+const path = require("path")
 
-app.set("view engine", "ejs"); // set view engine
-app.set("views", path.join(__dirname, "../views"));
-app.use(express.urlencoded({ extended: true })); //encode url
+app.use(express.static("/public")); //serve static files from "public" directory
 
 /*---------
 HTTP Server
@@ -24,46 +21,29 @@ socket.io Server
 --------------*/
 
 const { Server } = require("socket.io");
-const { default: Player } = require("./player");
-const { default: Ball } = require("./ball");
-const sio = new Server(http); //create new socket.io server, attach it to http server
+const io = new Server(http); //create new socket.io server, attach it to http server
 
-const playerList = {}; 
-
-sio.on("connection", (socket) => { //on connection to socket.io server
-    
+io.on("connection", (socket) => { //on connection to socket.io server
     console.log(`User connected: ${socket.id}`);
-    playerList[socket.id] = new Player(socket.id); //create new player
-    userList(sio); //update user list on new connection
-    
-    sio.on("close", () => console.log(`${socket.id} has disconnected.`)); //player disconnects
+
+    socket.on("paddleMove", (data) => { //paddle movements synced on both connections
+        socket.broadcast.emit("paddleMove", data);
+    });
+
+    socket.on("ballMove", (data) => { //ball movements synced on both connections
+        socket.broadcast.emit("ballMove", data);
+    });
+    io.on("close", () => {
+        ; //player disconnects
         console.log(`${socket.id} has disconnected.`);
-        delete playerList[socket.id]; //remove player from list
     });
-
-    sio.on("currentPlayers", (players) => { //update current players list
-        for (let id in players) {
-            if (players[id] === socket.id) {
-                playerList[socket.id].id = players[id]; //assign new player's ID
-            };
-        };
-    });
-
-    sio.on("move", (data) => {
-        movePlayer(socket.id, data);
-    });
-
-function userList(io) {
-    const users = Object.keys(playerList); //get connected users' IDs
-    console.log("Connected users:", users);
-    io.emit("updateUserList", users); //emit the updated list to clients
-}
+});
 
 /*---------------
 GET/POST Requests
 ---------------*/
 
 //handle index
-app.get("/", (req, res) => {
-    res.render("game");
+app.get('/', (req, res) => {
+    res.sendFile("index.html")
 });
