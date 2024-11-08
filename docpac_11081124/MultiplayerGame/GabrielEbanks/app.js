d33df70e68eee3c7086831314ef7ce9168b1b599
GEbanks <167536152+GEbanks@users.noreply.github.com>
2024-11-08 15:02:32 -0500

    work

--- docpac_11081124/MultiplayerGame/GabrielEbanks/app.js
+++ docpac_11081124/MultiplayerGame/GabrielEbanks/app.js
@@ -10,8 +10,32 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('game'); // Renders the game view
+    res.render('login'); // Renders the login view
 });
 
+app.get('/game', (req, res) => {
+    if (req.query.username) {
+        res.render('game', { name: req.query.username }); // Renders the game view
+    } else {
+        res.render('login'); // Renders the login view
+    }
+});
+
+
+wss.on('connection', ws => {
+    console.log('New client connected!')
+    ws.send(JSON.stringify({ user: "server", message: 'connection established' }))
+    ws.on('close', () => console.log('Client has disconnected!'))
+    ws.onerror = function () {
+        console.log('websocket error')
+    }
+
+})
+
+
+
+
+
+
 const http = require('http').Server(app);
-http.listen(3001, () => { console.log('Server started on http://localhost:3001'); })
+http.listen(3001, () => { console.log('Http listen server started on http://localhost:3001'); })
