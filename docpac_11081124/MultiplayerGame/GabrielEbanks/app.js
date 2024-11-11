3d7098bc93df786d5bf1c32685488290263f42c8
GEbanks <167536152+GEbanks@users.noreply.github.com>
2024-11-10 23:15:54 -0500

    011 almost done

--- docpac_11081124/MultiplayerGame/GabrielEbanks/app.js
+++ docpac_11081124/MultiplayerGame/GabrielEbanks/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 443 });
+let clients = new Map();
+let scores = []
+let gameStarted = false;
+
+
 
 app.set('view engine', 'ejs');
 
@@ -22,18 +27,69 @@ app.get('/game', (req, res) => {
 });
 
 
-wss.on('connection', ws => {
-    console.log('New client connected!')
-    ws.send(JSON.stringify({ user: "server", message: 'connection established' }))
-    ws.on('close', () => console.log('Client has disconnected!'))
-    ws.onerror = function () {
-        console.log('websocket error')
-    }
-
-})
 
 
+wss.on('connection', (ws) => {
+    console.log('New client connected!');
+    ws.send(JSON.stringify({ user: "server", message: 'connection established' }));
+    ws.onerror = () => {
+        console.log('WebSocket error');
+    };
+    ws.on('message', (data) => {
+        const message = JSON.parse(data);
 
+        if (message.type == 'playerUsername') {
+            const username = message.username;
+            clients.set(ws, username);
+            console.log(`${username} connected`);
+        }
+
+        if (message.type === 'startGame' && !gameStarted) {
+            gameStarted = true;
+            broadcast({ type: 'startCountdown', timeLeft: 20 });
+        }
+
+        if (message.type == 'scoreUpdate') {
+            const username = message.username;
+            // make that individual users score go up and be saved in the scores array
+        }
+
+        // if (message.type == 'endGame') {
+        //     endGame();
+        // }
+    });
+
+    ws.on('close', () => {
+        const username = clients.get(ws);
+        console.log(`${username} disconnected`);
+        clients.delete(ws);
+    });
+});
+
+
+function broadcast(data) {
+    wss.clients.forEach(client => {
+        if (client.readyState === client.OPEN) {
+            client.send(JSON.stringify(data));
+        }
+    });
+}
+
+// function endGame() {
+//     gameStarted = false;
+
+//     let winner = 
+
+//     wss.clients.forEach(client => {
+//         const username = message.username; {
+//             if (username == winner) {
+//                 client.send(JSON.stringify({ type: 'gameOver', result: 'You win' }));
+//             } else {
+//                 client.send(JSON.stringify({ type: 'gameOver', result: 'You lose' }));
+//             }
+//         }
+//     });   
+// }
 
 
 
