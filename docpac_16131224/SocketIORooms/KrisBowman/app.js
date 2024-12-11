17186aa0ab59e56df982e6d19ca4a2207f7a5be3
kris26658 <129105342+kris26658@users.noreply.github.com>
2024-12-11 11:35:43 -0500

    SocketIO Rooms update 2
    a lot of important fixes.

--- docpac_16131224/SocketIORooms/KrisBowman/app.js
+++ docpac_16131224/SocketIORooms/KrisBowman/app.js
@@ -1,13 +1,13 @@
-//to install required modules, in terminal: "npm i express ejs sqlite3 crypto jsonwebtoken express-session socket.io"
+//to install required modules, in terminal: "npm i express ejs sqlite3 crypto express-session socket.io"
 
 const express = require("express"); //import express
 const ejs = require("ejs"); //import ejs
 const sqlite3 = require("sqlite3"); //import sqlite3
 const crypto = require("crypto"); //import crypto
-const jwt = require("jsonwebtoken"); //import jsonwebtoken
 const session = require("express-session"); //import session
 const http = require("http"); //import http
 const socketIO = require("socket.io"); //import socket.io
+const { ADDRGETNETWORKPARAMS } = require("dns");
 
 const app = express(); //initialize express as the "app" object
 const SERVER = http.createServer(app); //create HTTP server
@@ -44,33 +44,98 @@ Socket.io Config
 --------------*/
 
 //handle Socket.io Connection
-
 io.on("connection", (socket) => {
+    const defaultRoom = "general";
+    var rooms = [defaultRoom]; //create rooms list
+
     console.log("A new user connected");
 
     //listen for client login
     socket.on("login", (username) => {
-        db.get(`SELECT * FROM users WHERE username = ?`, username, (err, user) => {
+        db.get(`SELECT * FROM users WHERE username = ?`, [username], (err, user) => {
             if (err) {
                 console.error(err);
+                socket.emit("chat message", "Database error.");
+            } else if (!user) {
+                socket.emit("chat message", "User not found.");
             } else {
-                socket.userId = user.id;
                 socket.username = user.username;
-                socket.emit("loginSuccess", { user: user.username });
-            }
+                socket.join(defaultRoom); //ensure the user joins the default room
+                
+                socket.emit("loginSuccess", {
+                    username: socket.username
+                });
+
+                socket.emit("chat message", `You have joined the default room: ${defaultRoom}`);
+                console.log(`${socket.username} has connected, and joined ${defaultRoom}`);
+            };
         });
     });
 
+    socket.emit("rooms", rooms); //emit initial rooms list to client
+
     //listen for chat messages from client
     socket.on("chat message", (msg) => {
-        console.log("Message: " + msg);
-        io.emit("chat message", msg); //broadcast message to all clients
+        if (/^\//.test(msg)) {
+            [command, arg] = msg.slice(1).split(" "); //remove "/" and split by space
+
+            switch (command) {
+                case "join":
+                    if (arg) {
+                        socket.join(arg); //join the given room
+                        socket.room = arg; //save room on the client
+                        io.emit("chat message", `Joined room: ${arg}`); //send confirmation message
+                    } else {
+                        socket.emit("chat message", "Command usage: /join [room]") //send command structure
+                    };
+                    break;
+                case "leave":
+                    if (socket.room) {
+                        if (arg) {
+                            socket.leave(arg) //leave the given room
+                            socket.to(socket.room).emit("chat message", `${socket.username} left the room.`);
+                        } else {
+                            socket.emit("chat message", "Command usage: /leave [room]") //send command structure
+                        };
+                    };
+                    break;
+                case "users":
+                    if (socket.room) {
+                        const usersInRoom = getUsersInRoom(socket.room); // Implement this function to get users in the room
+                        socket.emit("chat message", `Users in ${socket.room}: ${usersInRoom.join(", ")}`);
+                    } else {
+                        socket.emit("chat message", "You are not in a room.");
+                    };
+                    break;
+
+                default:
+                    socket.emit("chat message", "Unknown command.");
+            };
+        } else {
+            //broadcast message to users in the same room (if in a room)
+            if (socket.room) {
+                io.to(socket.room).emit("chat message", msg);
+            } else {
+                //notify user to join a room
+                socket.emit("chat message", "Join a room to send messages.");
+            };
+        };
     });
 
     //listen for client disconnect
     socket.on("disconnect", () => {
         console.log(socket.username + " disconnected");
     });
+
+    function getUsersInRoom(room) {
+        const clients = io.sockets.adapter.rooms.get(room) || new Set();
+        const users = [];
+        clients.forEach((clientId) => {
+            const clientSocket = io.sockets.sockets.get(clientId);
+            if (clientSocket && clientSocket.username) users.push(clientSocket.username);
+        });
+        return users;
+    };
 });
 
 /*------------
