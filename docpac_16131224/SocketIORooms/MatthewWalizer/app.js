5cf01f5d092c913d8fb9cba8619c33462f63a527
matth26676 <123019134+matth26676@users.noreply.github.com>
2024-12-12 14:56:36 -0500

    SocketIORooms update

--- docpac_16131224/SocketIORooms/MatthewWalizer/app.js
+++ docpac_16131224/SocketIORooms/MatthewWalizer/app.js
@@ -11,23 +11,23 @@ const server = app.listen(port, () => {
 });
 const io = socketIo(server);
 
-const FBJS_URL = 'http://172.16.3.100:420'
+const FBJS_URL = 'https://formbar.yorktechapps.com'
 var THIS_URL = ''
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) next()
-        else {
-    THIS_URL = 'http://' + req.headers.host + req.url + 'login'
-    res.redirect(`${FBJS_URL}/oauth?redirectURL=${THIS_URL}`)
-}
+    else {
+        THIS_URL = 'http://' + req.headers.host + req.url + 'login'
+        res.redirect(`${FBJS_URL}/oauth?redirectURL=${THIS_URL}`)
+    }
 }
 
-function checkRoomsList (socket) {
+function checkRoomsList(socket) {
     var roomsList = []
-    
+
     Array.from(socket.rooms).forEach(room => roomsList.push(room));
-    
-    for (room in roomsList) { 
+
+    for (room in roomsList) {
         if (roomsList[room] == socket.id) {
             roomsList.splice(room, 1);
         }
@@ -57,10 +57,10 @@ app.get('/login', (req, res) => {
         console.log(tokenData)
         req.session.token = tokenData;
         req.session.user = tokenData.username;
-        req.session.userid = tokenData.id;  
-              
+        req.session.userid = tokenData.id;
 
-        
+
+
         res.redirect('/');
     } else {
         res.redirect(`${AUTH_URL}?redirectURL=${THIS_URL}`);
@@ -83,12 +83,12 @@ io.use((socket, next) => {
 io.on('connection', (socket) => {
     socket.join('general');
     socket.request.session.selectedRoom = 'general';
-    socket.emit('roomsList', {roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom});
-    socket.emit('message', {roomName: 'general', content: 'Welcome to the chat room! Use /help for a list of commands'});
+    socket.emit('roomsList', { roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom });
+    socket.emit('message', { roomName: 'general', content: 'Welcome to the chat room! Use /help for a list of commands' });
     socket.on('message', (message) => {
         if (message.selectedRoom) {
             socket.request.session.selectedRoom = message.selectedRoom;
-            socket.emit('roomsList', {roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom});
+            socket.emit('roomsList', { roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom });
         } else {
             if (message.content.startsWith('/')) {
                 // grabs the message and splits it into command and arguments
@@ -99,26 +99,41 @@ io.on('connection', (socket) => {
                 switch (command) {
                     case 'join':
                         if (args.length === 0) {
-                            socket.emit('message', {roomName: 'general', content: 'No arguments provided for the command'});
+                            socket.emit('message', { roomName: 'general', content: 'No arguments provided for the command' });
                             return;
                         }
-                        socket.join(args[0]);
-                        socket.emit('roomsList', {roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom});
-                        socket.emit('message', {roomName: 'general', content: `You have joined ${args[0]}`});
-                        socket.to(args[0]).emit('message', {roomName: args[0], content: `${socket.request.session.user} has joined the room`});
+
+                        // Filters the `args` array to remove duplicate elements and assigns the result to `joinargs`.
+                        let joinargs = args.filter((arg, index) => args.indexOf(arg) === index);
+                        joinargs.forEach(arg => {
+                            socket.join(arg);
+                            socket.emit('roomsList', { roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom });
+                            socket.to(arg).emit('message', { roomName: arg, content: `${socket.request.session.user} has joined the room` });
+                        });
+                        let joinedRooms = joinargs.join(', ');
+                            socket.emit('message', { roomName: 'general', content: `You have joined: ${joinedRooms}` });
                         break;
                     case 'leave':
                         if (args.length === 0) {
-                            socket.emit('message', {roomName: 'general', content: 'No arguments provided for the command'});
-                            return;
-                        } else if (args[0] === 'general') {
-                            socket.emit('message', {roomName: 'general', content: 'There\'s no escape'});
+                            socket.emit('message', { roomName: 'general', content: 'No arguments provided for the command' });
                             return;
                         }
-                        socket.leave(args[0]);
-                        socket.emit('roomsList', {roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom});
-                        socket.emit('message', {roomName: 'general', content: `You have left ${args[0]}`});
-                        socket.to(args[0]).emit('message', {roomName: args[0], content: `${socket.request.session.user} has left the room`});
+                        // Filters the `args` array to remove duplicate elements and assigns the result to `leaveargs`.
+                        let leaveargs = args.filter((arg, index) => args.indexOf(arg) === index);
+                        leaveargs.forEach(arg => {
+                            if (arg === 'general') {
+                                socket.emit('message', { roomName: 'general', content: 'There\'s no escape' });
+                                leaveargs.splice(leaveargs.indexOf(arg), 1);
+                            } else {
+                                socket.leave(arg);
+                                socket.emit('roomsList', { roomsList: checkRoomsList(socket), selectedRoom: socket.request.session.selectedRoom });
+                                socket.to(arg).emit('message', { roomName: arg, content: `${socket.request.session.user} has left the room` });
+                            }
+                        });
+                        if (leaveargs.length != 0) {
+                            let leftRooms = leaveargs.join(', ');
+                            socket.emit('message', { roomName: 'general', content: `You have left: ${leftRooms}` });
+                        }
                         break;
                     case 'users':
                         let users = [];
@@ -132,7 +147,7 @@ io.on('connection', (socket) => {
                                 }
                             });
                         }
-                        socket.emit('message', {roomName: args[0], content: `Users in ${socket.request.session.selectedRoom}: ${users.join(', ')}`});
+                        socket.emit('message', { roomName: args[0], content: `Users in ${socket.request.session.selectedRoom}: ${users.join(', ')}` });
                         break;
                     case 'rooms':
                         let rooms = [];
@@ -147,40 +162,40 @@ io.on('connection', (socket) => {
                                 rooms.push(room);
                             }
                         });
-                        socket.emit('message', {roomName: 'general', content: `Rooms: ${rooms.join(', ')}`});
+                        socket.emit('message', { roomName: 'general', content: `Rooms: ${rooms.join(', ')}` });
                         break;
                     case 'help':
                         if (args.length === 0) {
-                        socket.emit('message', {roomName: 'general', content: 'Available commands: /join, /leave, /users, /rooms, /help. Use /help <command> for more information'});
+                            socket.emit('message', { roomName: 'general', content: 'Available commands: /join, /leave, /users, /rooms, /help. Use /help <command> for more information' });
                         } else {
                             switch (args[0]) {
                                 case 'join':
-                                    socket.emit('message', {roomName: 'general', content: 'Join a room. Usage: /join <room>'});
+                                    socket.emit('message', { roomName: 'general', content: 'Join the room(s). Usage: /join <room> or /join <room1> <room2>...' });
                                     break;
                                 case 'leave':
-                                    socket.emit('message', {roomName: 'general', content: 'Leave a room. Usage: /leave <room>'});
+                                    socket.emit('message', { roomName: 'general', content: 'Leave the room(s). Usage: /leave <room> or /leave <room1> <room2>...' });
                                     break;
                                 case 'users':
-                                    socket.emit('message', {roomName: 'general', content: 'List users in the room the user is in. Usage: /users'});
+                                    socket.emit('message', { roomName: 'general', content: 'List users in the room the user is in. Usage: /users' });
                                     break;
                                 case 'rooms':
-                                    socket.emit('message', {roomName: 'general', content: 'List all active rooms. Usage: /rooms'});
+                                    socket.emit('message', { roomName: 'general', content: 'List all active rooms. Usage: /rooms' });
                                     break;
                                 case 'help':
-                                    socket.emit('message', {roomName: 'general', content: 'Get help. Usage: /help <command>'});
+                                    socket.emit('message', { roomName: 'general', content: 'Get information of all commands or information on a specific command. Usage: /help <command>' });
                                     break;
                                 default:
-                                    socket.emit('message', {roomName: 'general', content: 'Unknown command'});
+                                    socket.emit('message', { roomName: 'general', content: 'Unknown command' });
                                     break;
                             }
                         }
                         break;
                     default:
-                        socket.emit('message', {roomName: 'general', content:'Unknown command'});
+                        socket.emit('message', { roomName: 'general', content: 'Unknown command' });
                         break;
                 }
             } else {
-                io.to(message.receivingRoom).emit('message', {sender: socket.request.session.user, date: message.currentTime, roomName: message.receivingRoom, content: message.content});
+                io.to(message.receivingRoom).emit('message', { sender: socket.request.session.user, date: message.currentTime, roomName: message.receivingRoom, content: message.content });
             }
         }
     });
