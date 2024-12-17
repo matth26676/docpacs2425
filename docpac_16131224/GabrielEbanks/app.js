0fe1ba76c9a531f1a6e4923707dfc371965fabed
GEbanks <167536152+GEbanks@users.noreply.github.com>
2024-12-16 21:45:14 -0500

    /users broken

--- docpac_16131224/GabrielEbanks/app.js
+++ docpac_16131224/GabrielEbanks/app.js
@@ -5,6 +5,9 @@ const session = require('express-session');
 const sqlite3 = require('sqlite3');
 const socketIo = require('socket.io');
 const PORT = 3000;
+users = new Set();
+const roomUsers = {};
+
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +28,7 @@ function isAuthenticated(req, res, next) {
 }
 
 app.get('/', isAuthenticated, (req, res) => {
-    res.render('index', { user: req.session.user,});
+    res.render('index', { user: req.session.user, });
 });
 
 app.get('/login', (req, res) => {
@@ -41,7 +44,7 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/', isAuthenticated, (req, res) => {
-    console.log('Session user:', req.session.user); 
+    console.log('Session user:', req.session.user);
     res.render('index', { user: req.session.user });
 });
 
@@ -53,50 +56,74 @@ const server = app.listen(PORT, () => {
 
 const io = socketIo(server);
 io.on('connection', (socket) => {
-
     console.log('New client connected');
     socket.join('general');
     console.log('joined general');
+    users.add(socket.id);
 
 
     var room = socket.rooms;
-    users = new Set();
+
     console.log(room);
+
+    socket.on('useForRooms', () => {
+        const rooms = Array.from(socket.rooms);
+        socket.emit('userList', {
+            rooms: rooms
+        });
+    });
 
     socket.on('joinRoom', (data) => {
         const user = data.user;
         const arg = data.arg;
         socket.join(arg);
         currentRoom = arg;
+
+        if (!roomUsers[arg]) {
+            roomUsers[arg] = new Set();
+        }
+
+        roomUsers[arg].add(user);
         console.log(`username is: ${user}, and they are joining ${arg}`);
         console.log(`Rooms for socket ${socket.id}:`, Array.from(socket.rooms));
+        
+        const currentRooms = Array.from(socket.rooms); 
+        socket.emit('roomListUpdate', {
+            message: `You joined room: ${arg}. Your current rooms: ${currentRooms.join(', ')}`,
+            rooms: currentRooms
+        });
     });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
+        users.delete(socket.id);
     });
 
     socket.on('userList', (data) => {
         console.log('Client requested user list');
-        io.to(data.room).emit('userList', Array.from(users));
-
+        const roomUsers = Array.from(users);
+        io.to(data.room).emit('userList', roomUsers);
     });
 
     socket.on('message', (data) => {
-        console.log (data);
+        console.log(data);
         const room = data.room;
         const message = data.message
         const user = data.user;
         console.log(room, message, user);
         console.log('message: received in room', room);
-        io.to(room).emit('message', user, message, );
+        io.to(room).emit('message', user, message,);
     });
 
     socket.on('leave', (data) => {
         const arg = data.arg;
         socket.leave(arg);
-        console.log(`Rooms for socket ${socket.id}:`, Array.from(socket.rooms));
-
-});
+        console.log(`User left room: ${arg}`);
+        const currentRooms = Array.from(socket.rooms);
+        socket.emit('roomListUpdate', {
+            message: `Your current rooms: ${currentRooms}`,
+            rooms: currentRooms
+        });
+    });
 });
 
