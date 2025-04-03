0906fb4370d5eea14bdc632ebb84441db8fcde10
Tyler Morgan <102384418+TYLERMARTIN1234@users.noreply.github.com>
2025-04-03 11:19:10 -0400

    betrter thing

--- docpac_30280325/ConsoleDataEntryProgram2/TylerMorgan.js
+++ docpac_30280325/ConsoleDataEntryProgram2/TylerMorgan.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 var orders = [];
-var items = [];
+var order = { name: "", home: "", items: [], totals: {} }
 while (true) {
     var Q1 = prompt("1:make new profile, 2:edit orders, 3:show all orders, 4:GET OUT | ")
 
@@ -25,7 +25,6 @@ while (true) {
 function newUser() {
     var inputName = prompt("name for your order?")
     var inputHome = prompt("where is your FAKE house?")
-    var order = { name: "", home: "", items:[] }
     order.name = inputName
     order.home = inputHome
     orders.push(order)
@@ -33,22 +32,23 @@ function newUser() {
 function editOrder() {
     let names = []
     for (order in orders) {
-        names.push(orders[order].name + (Number.parseInt(order)+1))
+        names.push(orders[order].name + (Number.parseInt(order) + 1))
     }
     console.log(names);
-    
+
     var checkUser = prompt("which profile would you like to edit? (number plz) | ")
-    var order = orders[Number.parseInt(checkUser) -1]
-    
+    var order = orders[Number.parseInt(checkUser) - 1]
+
     var Q2 = prompt("1:add items, 2:finish edit, 3:GET OUT | ")
     switch (Q2) {
         case "1":
             var item = {
-                name:"",
-                amount:0,
-                price:0,
-                retail:0
+                name: "",
+                amount: 0,
+                price: 0,
+                retail: 0
             }
+
             item.name = prompt("what item would you like?")
             var newAmount = Number.parseFloat(prompt("how many?"))
             item.amount = newAmount
@@ -56,15 +56,36 @@ function editOrder() {
             item.price = itemPrice
             var retailPrice = Number.parseFloat(prompt("how much do we sell the item for?"))
             item.retail = retailPrice
-            order.items = item
-            console.log(orders);
+            order.items.push(item)
             break;
         case "2":
-            var profit = 0
-            var subTotal = 0
-            var shippingFee = 0
-            var salesTax = 0
-            order.items.forEach(addItems());
+                var profit = 0
+                var subTotal = 0
+                var shippingFee = 0
+                var salesTax = 0
+                var total = 0
+
+            for (item of order.items) {
+                let itemPrice = item.retail * item.amount
+                subTotal += itemPrice
+
+                let gain = item.retail - item.price
+                profit += gain
+            }
+            salesTax = subTotal * .06
+            salesTax = salesTax.toFixed(2)
+            salesTax = Number.parseFloat(salesTax)
+            if (subTotal < 50) {
+                shippingFee = 5
+            }
+            
+            total = subTotal + salesTax + shippingFee
+            order.totals.profit = profit
+            order.totals.subTotal = subTotal
+            order.totals.shippingFee = shippingFee
+            order.totals.salesTax = salesTax
+            order.totals.total = total
+            
             break;
         case "3":
             return;
@@ -72,15 +93,7 @@ function editOrder() {
             console.log("nuh uh");
             break;
     }
-    function addItems() {
-        //add retail price of item by quantity
-        var itemPrice = orders.items.retail * orders.items.amount
-        //add itemPrice to subTotal 
-        console.log(itemPrice);
-        //multiply subtotal by .06
-        //assign the value to salesTax
-    }
 }
 function showOrders() {
-    
+
 }
